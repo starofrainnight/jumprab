eb7e93004578a75a711ce3c2b0b56dab79e4cb42
Hong-She Liang <sorn@taorules.com>
2022-09-27 23:11:01 +0800

    Ensure script run as root

--- jumprab/__main__.py
+++ jumprab/__main__.py
@@ -3,6 +3,8 @@
 
 """Console script for jumprab."""
 
+import os
+import sys
 import yaml
 import click
 from .jumprab import JumpRab
@@ -12,6 +14,11 @@ from .jumprab import JumpRab
 @click.option("-c", "--config", required=True)
 def main(config):
     """JumpRab, a developer's mirrors & proxies batch settings program."""
+
+    # This script must be run as root!
+    if not os.geteuid() == 0:
+        sys.exit("This script must be run as root!")
+
     with open(config, "r") as f:
         cfg = yaml.load(f, yaml.FullLoader)
 
